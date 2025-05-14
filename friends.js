6d193accb3e5b0b5adeec22414568127a3859338
siderilla <167792687+siderilla@users.noreply.github.com>
2025-05-14 10:02:29 +0200

    filter best practice

--- friends.js
+++ friends.js
@@ -5,3 +5,8 @@ function friend(friends){
 
 console.log(friend(["Ryan", "Kieran", "Mark"]));
 console.log(friend(["Ryan", "Jimmy", "123", "4", "Cool Man"]));
+
+// BEST PRACTICE:
+// function friend(friends){
+//   return friends.filter(n => n.length === 4)
+// }
