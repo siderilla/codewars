b4cf1ddb6fdd4a6666af685a3b0857fd8242baa0
siderilla <siderilla11@gmail.com>
2025-08-31 21:05:55 +0200

    empty array is truthy

--- likes.js
+++ likes.js
@@ -1,23 +1,28 @@
 function likes(names) {
 
-	if (!names) {
-		return console.log("no one likes this");
-	}
-	for (let i = 0; i < names.length; i++) {
-		const element = names[i];
-		if (names.length == 1) {
-			console.log(names[0], "likes this")
-		} else if (names.length == 2) {
-			console.log(names[0], "and", names[1], "like this")
-		} else if (names.length == 3) {}
-
-	}
+	if (!names || names.length === 0) {
+		return `no one likes this`;
+	} else if (names.length === 1) {
+		return `${names[0]} likes this`;
+	} else if (names.length === 2) {
+		return `${names[0]} and ${names[1]} like this`;
+	} else if (names.length === 3) {
+		return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+	} else {
+		return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+	} 
 
 }
 
-console.log(likes());
-console.log(likes(["Peter"]));
+
+console.log(likes([]));
+console.log(likes(['Peter']));
 console.log(likes(['Jacob', 'Alex']));
-console.log(likes())
-console.log(likes())
-console.log(likes())
+console.log(likes(['Max', 'John', 'Mark']))
+console.log(likes(['Alex', 'Jacob', 'Mark', 'Max']))
+console.log(likes(['Jan', 'Bob', 'Kitty', 'Cole', 'Barnie', 'Olly']))
+console.log(likes(['Kitty', 'Cole', 'Barnie', 'Olly', 'Jan', 'Bob', 'Max', 'John', 'Mark']))
+
+
+// al posto dell'interpolazione si poteva usare il metodo switch case
+// l'array vuoto in js è valore truthy, dunque non rientra tra i casi null o undefined
